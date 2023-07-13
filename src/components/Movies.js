00e51608874c639fa88e95f1bcb766b94bf8f958
sandyjtech <129788051+sandyjtech@users.noreply.github.com>
2023-07-13 16:51:22 -0700

    Update Movies.js

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,18 +3,25 @@ import { movies } from "../data";
 
 
 function Movies() {
-  return <div>
-    <h1>Movies Page</h1>
-    {movies.map((movie) => (
-      <div key={movie.title}>
-        <h2>{movie.title}</h2>
-        <ul>{movie.genres.map((genre) => (
-          <li key={genre}>{genre}</li>
-        ))}</ul>
-      </div>
-    ))}
-    
-  </div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {/* Iterate over each movie in the `movies` array */}
+      {movies.map((movie) => (
+        <div key={movie.title}>
+          <h2>{movie.title}</h2>
+          <h2>{movie.time}</h2>
+          {/* Iterate over each genre in the `genres` array of the current movie */}
+          <ul>
+            {movie.genres.map((genre) => (
+              <li key={genre}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Movies;
+
